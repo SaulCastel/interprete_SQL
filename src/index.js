cf2cb4b787584edfb6274fc5e11c78e75fcc6f28
Saul <saulcastel018@gmail.com>
2023-10-05 19:55:53 -0600

    Limpiar raiz de directorios

--- src/index.js
+++
@@ -1,9 +0,0 @@
-const parser = require('./parser/query_parser.js')
-const genDOT = require('./util/Graph.js')
-
-const input = 'print "hola, que tal?"; print 2+3*4; print "conca" + "tenacion";'
-const stmts = parser.parse(input)
-console.log(genDOT(stmts))
-for(const stmt of stmts){
-    stmt.interpret()
-}
--- src/util/String.js
+++
@@ -1,9 +0,0 @@
-function escapeStr(str){
-    return str.replaceAll('\\"', '"')
-        .replaceAll('\\n', '\n')
-        .replaceAll('\\t', '\t')
-        .replaceAll("\\'", '\'')
-        .replaceAll("\\\\", '\\')
-}
-
-module.exports = escapeStr
